ceeda4d42265468bf6fc60677d746e91b085d819
Chalermrat Choeisai <schalermrat@gmail.com>
2019-11-26 15:16:24 +0700

    Init express app

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Application } from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+class App {
+  public app: Application
+
+  constructor() {
+    this.app = express()
+    this.setConfig()
+  }
+
+  private setConfig() {
+    this.app.use(bodyParser.json({ limit: "50mb " }))
+    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }))
+    this.app.use(cors())
+  }
+}
+
+export default new App().app
